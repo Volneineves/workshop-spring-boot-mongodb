7883444b10d4e77827a0040b541f1094ae880f16
volneineves <volneinevesfilho@gmail.com>
2021-10-13 22:09:09 -0300

    Obtendo um usuario por id

--- src/main/java/com/volneineves/workshopmongo/services/UserService.java
+++ src/main/java/com/volneineves/workshopmongo/services/UserService.java
@@ -2,10 +2,14 @@ package com.volneineves.workshopmongo.services;
 
 import com.volneineves.workshopmongo.domain.User;
 import com.volneineves.workshopmongo.repository.UserRepository;
+import com.volneineves.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +19,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
