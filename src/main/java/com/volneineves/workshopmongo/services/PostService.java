882a32552c452d8c4c5b73d42c47ca1a56c04c72
volneineves <volneinevesfilho@gmail.com>
2021-10-14 09:50:13 -0300

    Obtendo um post por id

---
+++ src/main/java/com/volneineves/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.volneineves.workshopmongo.services;
+
+import com.volneineves.workshopmongo.domain.Post;
+import com.volneineves.workshopmongo.repository.PostRepository;
+import com.volneineves.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+    }
+}
