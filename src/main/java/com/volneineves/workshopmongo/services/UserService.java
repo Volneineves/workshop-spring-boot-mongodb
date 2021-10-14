d82e5d32530ae2921d7925752347ef63d0e33f53
volneineves <volneinevesfilho@gmail.com>
2021-10-13 22:47:44 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/volneineves/workshopmongo/services/UserService.java
+++ src/main/java/com/volneineves/workshopmongo/services/UserService.java
@@ -6,8 +6,6 @@ import com.volneineves.workshopmongo.repository.UserRepository;
 import com.volneineves.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,9 +29,13 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public void delete(String id){
+        findById(id); // Se ele não achar o id ele já mandar a exceção de uma vez
+        repo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDto){
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
-
 
 }
