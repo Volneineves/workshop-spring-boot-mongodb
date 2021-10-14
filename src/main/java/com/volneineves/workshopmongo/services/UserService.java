658a383ca6700fb4b13739efb6950795b803698c
volneineves <volneinevesfilho@gmail.com>
2021-10-13 22:31:46 -0300

    Insercao de usuario com POST

--- src/main/java/com/volneineves/workshopmongo/services/UserService.java
+++ src/main/java/com/volneineves/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.volneineves.workshopmongo.services;
 
 import com.volneineves.workshopmongo.domain.User;
+import com.volneineves.workshopmongo.dto.UserDTO;
 import com.volneineves.workshopmongo.repository.UserRepository;
 import com.volneineves.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,4 +26,14 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
+
 }
